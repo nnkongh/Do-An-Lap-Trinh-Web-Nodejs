a3852c6543214d16d18a9c5775b65340a40c3a05
nnkongh <haukong1308@gmail.com>
2025-04-12 20:45:42 +0700

    BookManagement

---
+++ client/src/store/slices/bookSlice.js
@@ -0,0 +1,4 @@
+// 5:36:00 có thêm export nữa nhé ní
+// [có code phần bookSlice này thì code xong rồi qua BookManagement dòng 34 tới dòng 37
+// để kiểm tra lại tên hàm bên kia có giống với tên hàm của trang này 
+// không nhé (XONG PHẦN NÀY RỒI HẴN KIỂM TRA) rồi viết thêm cái export ở tui vừa đề cập ở trên] 
